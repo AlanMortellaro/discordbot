9ade27103b4cec1057f58a6362ea3c1a12d06bed
AlanMortellaro <alan.mortellaro@hotmail.com>
2017-11-27 19:21:27 +0100

    add function cmd_geo <- (take json content from web page)

--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     os = require("os"),
-    jsonfile = require('jsonfile')
+    jsonfile = require('jsonfile'),
+    http = require('http')
 
 var commands = {
   cmd_ping: function(command, message){
@@ -88,7 +89,68 @@ var commands = {
 
 
 
+  },
+
+  cmd_geo: function(command, message){
+
+    function getJSON(options, cb){
+      http.request(options, function(res){
+          var body = '';
+          res.on('data', function(chunk){
+            body+=chunk
+          });
+          res.on('end', function(){
+            var result = JSON.parse(body);
+            cb(null, result);
+          });
+
+          res.on('error', cb);
+      })
+      .on('error', cb)
+      .end();
+    }
+    var options = {
+        host: 'www.googleapis.com',
+        port: 80,
+        path: '/customsearch/v1?key=YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures',
+        method: 'GET'
+    };
+
+    getJSON(options, function(err, result){
+      if(err)
+      {
+        return console.log('Error while trying to get response: \n\n', err);
+      }
+      else
+      {
+          return console.log(result);
+      }
+    });
+/*
+    var srcGeo = "http://www.google.com/jsapi?key=AIzaSyCruLqWqu-oD2VHC3p-pASJNQ8c107BpMs"
+
+    if (google.loader.ClientLocation)
+    {
+        var latitude = google.loader.ClientLocation.latitude;
+        var longitude = google.loader.ClientLocation.longitude;
+        var city = google.loader.ClientLocation.address.city;
+        var country = google.loader.ClientLocation.address.country;
+        var country_code = google.loader.ClientLocation.address.country_code;
+        var region = google.loader.ClientLocation.address.region;
+
+        var text = 'Your Location<br /><br />Latitude: ' + latitude + '<br />Longitude: ' + longitude + '<br />City: ' + city + '<br />Country: ' + country + '<br />Country Code: ' + country_code + '<br />Region: ' + region;
+    }
+    else
+    {
+        var text = 'Google was not able to detect your location';
+    }
+
+    document.write(text);
+
+    geoTest();
+*/
   }
+
 }
 
 module.exports = commands;
